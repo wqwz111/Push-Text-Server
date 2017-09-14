2c8acbbf22c9be6a4844b314431b8064e553d7b9
wqwz111 <wqwz111@gmail.com>
2017-09-14 10:25:14 +0800

    update

---
+++ app.js
@@ -0,0 +1,73 @@
+var config = require('./config.json');
+var DbHelper = require('./db');
+var dbHelper = new DbHelper(config.db, null, null);
+var server = require('http').createServer();
+var io = require('socket.io')(server, {
+    serveClient: false,
+    pingInterval: 10000,
+    pintTimeout: 5000,
+    cookie: false
+});
+server.listen(process.env.PORT || config.port);
+
+
+io.on('connection', function (socket) {
+
+    socket.on('login', function (req, ack) {
+        dbHelper.createAnonyUser().then(function (user) {
+            socket.emit('logged-in', user);
+            ack && ack();
+        });
+    });
+
+    socket.on('logout', function() {
+        socket.emit('logged-out', null);
+    });
+
+    socket.on('enter-room', function (req, ack) {
+        var userId = req.user_id;
+        var newRoom = req.new_room;
+        dbHelper.getCurrentRoom(userId).then(function (currRoom) {
+            if (currRoom) {
+                var currRoomNo = currRoom.roomNo;
+                socket.leave(currRoomNo, function () {
+                    dbHelper.leaveRoom(currRoomNo, userId).then(function() {
+                        socket.join(newRoom, function () {
+                            dbHelper.enterRoom(newRoom, userId).then(function (room) {
+                                console.log("enter room " + newRoom + " success.");
+                                 ack && ack(room.messages);
+                            });
+                        });
+                    });
+                });
+            } else {
+                socket.join(newRoom, function () {
+                    dbHelper.enterRoom(newRoom, userId).then(function (room) {
+                        console.log("enter room " + newRoom + " success.");
+                        ack && ack(room.messages);
+                    });
+                });
+            }
+        });
+    });
+
+    socket.on('leave-room', function (req, ack) {
+        socket.leave(req.current_room, function (err) {
+            dbHelper.leaveRoom(req.current_room, req.user_id).then(function() {
+                console.log("leave room " + req.current_room + " success.");
+                ack && ack();
+            });
+
+        });
+    });
+
+    socket.on('new-message', function (req, ack) {
+        dbHelper.getCurrentRoom(req.user_id).then(function (currRoom) {
+            socket.emit('broadcast', req.msg);
+            dbHelper.pushMessage(currRoom.roomNo, req.msg).then (function (room) {
+                ack && ack(room.messages);
+            });
+        });
+    });
+
+});
---
+++ db.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('q').Promise;
+
+function DbHelper(dbUrl,errCallback, sucCallback) {
+    mongoose.connect(dbUrl || 'mongodb://localhost/test',
+        {useMongoClient: true}, function (err, res) {
+        if (err) {
+            if (typeof errCallback === 'function') {
+                errCallback(err);
+            }
+        } else {
+            if (typeof sucCallback === 'function') {
+                sucCallback(res);
+            }
+        }
+    });
+
+    /* Create Schema*/
+    this.userSchema = new mongoose.Schema({
+        name: String
+    });
+    this.massageSchema = new mongoose.Schema({
+        content: String,
+        fileUrl: String,
+        uid: mongoose.Schema.Types.ObjectId
+    });
+    this.roomSchema = new mongoose.Schema({
+        roomNo: String,
+        ownerId: mongoose.Schema.Types.ObjectId,
+        messages: [this.massageSchema],
+        users: [mongoose.Schema.Types.ObjectId],
+        createdTime: {type: Date, default: new Date()}
+    });
+
+    this.Room = mongoose.model('rooms', this.roomSchema);
+    this.User = mongoose.model('users', this.userSchema);
+}
+
+DbHelper.prototype.getUser = function (uid) {
+    return this.User.findOne({_id: uid}).exec();
+};
+
+DbHelper.prototype.createUser = function (username) {
+    var User = this.User;
+    var temp = new User({name: username});
+    return temp.save();
+};
+
+DbHelper.prototype.createAnonyUser = function () {
+    return this.createUser("nobody");
+};
+
+DbHelper.prototype.deleteUser = function (uid) {
+    return this.User.remove({_id: uid});
+};
+
+DbHelper.prototype.enterRoom = function (roomNumber, userId) {
+    var Room = this.Room;
+
+    var p1 = Room.findOne({roomNo: roomNumber}).exec();
+    return p1.then(function (room) {
+        var p2;
+        if (!room) {
+            var newRoom = new Room({
+                roomNo: roomNumber,
+                ownerId: userId,
+                messages: [],
+                users: [userId]
+            });
+            p2 = newRoom.save();
+        } else {
+            room.users.addToSet(userId);
+            p2 = room.save();
+        }
+        return p2;
+    })
+};
+
+DbHelper.prototype.leaveRoom = function (roomNumber, userId) {
+    return this.Room.findOneAndUpdate({roomNo: roomNumber},
+        {$pull: {users: new mongoose.mongo.ObjectId(userId)}},{new: true});
+};
+
+DbHelper.prototype.getCurrentRoom = function (userId) {
+    return this.Room.findOne({users: new mongoose.mongo.ObjectId(userId)}).exec();
+};
+
+DbHelper.prototype.clearUnusedRooms = function (someTimeAgo) {
+    var timeBefore = Date.now() - someTimeAgo;
+    return this.Room.deleteMany({createdTime: {$lte: timeBefore}}).exec();
+};
+
+DbHelper.prototype.pushMessage = function (roomNumber, message) {
+    var newMsg = {
+        content: message.content,
+        fileUrl: message.fileUrl,
+        uid: message.uid
+    };
+    return this.Room.findOneAndUpdate({roomNo: roomNumber},
+        {$addToSet: {messages: newMsg}}, {new: true}).exec();
+};
+
+module.exports = DbHelper;
